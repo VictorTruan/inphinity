f506cf85bd0d4e468461ffcb885d9512adf4adac
victortruan <victor.truan@heig-vd.ch>
2019-04-25 14:16:59 +0200

    Set Pfam et inph fonctionnel

--- projet_b11/import_databases/Pfam.py
+++ projet_b11/import_databases/Pfam.py
@@ -1,27 +1,40 @@
 from ftplib import FTP
+from projet_b11.import_databases.Domain_Interaction import DomainInteraction
 import gzip
-import array
 import shutil
 import os
 import re
+#TODO import de test
+from projet_b11.import_databases.REST_Domain_Interaction import RESTDomainInteraction
+import sys
+#TODO import de test
+
 # TODO Trouver un format pour pour interaction.
-# TODO Ouvrir la connexion avec la base de donnée inphinity
-# TODO Utiliser REST pour récuperer toutes les intéractions.
-# TODO Récuperer toutes les interactions sur le serveur FTP
+    ##--> Pas de classes sans SQL, creation d'une miniclasse
+# TODO Ouvrir la connexion avec la base de donnee inphinity
+    ##--> Fait par Nico
+# TODO Utiliser REST pour recuperer toutes les interactions.
+    ##-->Fait dans Rest_Pfam
+# TODO Recuperer toutes les interactions sur le serveur FTP
+    ##--> Fait, cette classe permet de retourner
 # TODO Mettre les interaction inphinity au bon format
+    ##-->Fait la classe contient un set
 # TODO Mettre les interactions Pfam au bon format
+    ##--> Fait, la classe contient un set.
 # TODO Comparer les resultats.
 # Dans un second temps
-# TODO Si différente
-    # TODO Trouver les diff et les insérer avec REST
+# TODO Si differente
+    # TODO Trouver les diff et les inserer avec REST
+        #TODO utiliser les set
     # TODO Passer toutes les prot a HMM
-    # TODO Trier tous les domaine de REST et HMM afin de trouver les nouveaux et les insérers
+    # TODO Trier tous les domaine de REST et HMM afin de trouver les nouveaux et les inserers
     # TODO Lancer les nouveaux calcules de scores.
-# TODO Sinon
-    #TODO Pas grand chose pour le moment.
 #TODO Trouver comment lancer automatiquement une maj.
 
-class pfam:
+"""
+Class used to get domain-domain interaction from Pfam
+"""
+class Pfam:
 
     server_adress = 'ftp.ebi.ac.uk'
 
@@ -29,10 +42,12 @@ class pfam:
         self.version = None
         self.ftp = FTP(self.server_adress)
         self.ftp.login()
-        self.interactions = []
+        self.domain_interactions = set()
 
-    # Méthode permettant d'obtenir la version de la base Pfam, elle est écrite dans un fichier version.txt
-    # L'attribut version est utilisé pour stocker la version sous la forme d'un double.
+
+    """
+    Function used to get the Pfam database version. self.version is used to keep it.
+    """
     # http://zetcode.com/python/ftp/ Pour la connexion FTP
     # https://stackoverflow.com/questions/48466421/python-how-to-decompress-a-gzip-file-to-an-uncompressed-file-on-disk
     # Pour l'ouverture du gz.
@@ -44,58 +59,56 @@ class pfam:
         os.remove("./tmp.gz")
         self.version = open('./version.txt','r').read()
         self.version = re.findall('\d+.\d+', self.version)[0]
-
-    # Méthode permettant d'obtenir toutes les interactions présente dans la base de donnée Pfam.
+    """
+    Function used to get all the domain-domain interactions from Pfam database.
+    All the interaction are in domain_interactions
+    """
     # https://bytes.com/topic/python/answers/870172-python-search-text-file-string-replace
-    # Pour l'utilisation de fileInput.
+    # For fileInput.
     # https://docs.python.org/2/library/fileinput.html
     def get_interactions(self):
-        # Telechargement de l'archive
+        # Downloads of the archive
         self.ftp.retrbinary("RETR /pub/databases/Pfam/current_release/database_files/pfamA_interactions.txt.gz",
                             open("./tmp.gz", "wb").write)
-        # Ouverture de l'archive
+        # Opening of the archive
         with gzip.open("./tmp.gz", 'rb') as f_in:
             with open('interactions_pfam.txt', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        # Suppression de l'archive
+        # Suppression of the archive
         os.remove("./tmp.gz")
         interactions = open('interactions_pfam.txt', 'rb')
-        # Lecture du fichier par lignes.
+        # We read the file line by line.
         for line in interactions.readlines():
-            # Separation des deux domaines d'une interaction
+            # Separations of both domain from the interaction
             line = line.decode('UTF-8')
+            line = line.strip('\n')
             sep = line.partition('\t')
-            sep[2].strip('\n')
-            # Creation de l'intéraction et àjout à une liste
-            self.interactions.append(Interaction(sep[0], sep[2]))
-        # Rangement des intéractions trouvées
-        self.interactions =  sorted(self.interactions, key=lambda inter: (inter.premier_dom+inter.second_dom))
-
+            # Instanciation of a new interaction and add it to domain_interactions
+            self.domain_interactions.add(DomainInteraction(sep[0], sep[2]))
+    """
+    Redefinition of toStringso it display all the interactions in Pfam Database.
+    """
     def __str__(self):
-        tmp = "Version : " + self.version + "\n Liste des intéractions:\n"
-        for inter in self.interactions:
+        tmp = "Version : " + self.version + "\n List of all interactions:\n"
+        for inter in self.domain_interactions:
             tmp += inter.__str__()
         return tmp
 
 
-class Interaction:
+#Test
+pfam = Pfam()
+pfam.get_interactions()
+inphi = RESTDomainInteraction()
+inphi.get_domain_dict()
+inphi.get_domain_inter()
+tmp = pfam.domain_interactions - inphi.domain_interactions
+print(str(len(pfam.domain_interactions)) + " interactions in pfam database")
+print(str(len(inphi.domain_interactions))+ " interactions in inph database")
+print(str(len(tmp)) + " interactions in pfam database but not in inph")
 
-    def __init__(self, premier_dom, second_dom):
-        self.premier_dom = premier_dom
-        self.second_dom = second_dom
-
-    # https://www.dimagi.com/blog/overriding-equals-in-python/ Pour comment redéfinir equals
-    def __eq__(self, obj):
-        return isinstance(obj, Interaction) and self.premier_dom == obj.premier_dom and\
-               self.second_dom == obj.second_dom
-
-    # Méthode pour afficher les domaines présent dans une intéraction
-    def __str__(self):
-        tmp = "Premier domaine : "+ self.premier_dom + "\nSecond domaine : "+ self.second_dom
-        return tmp
-
-
-fam = pfam()
-fam.get_version()
-fam.get_interactions()
-print(fam)
+for interaction in pfam.domain_interactions:
+    if interaction not in inphi.domain_interactions:
+        pass
+#TODO Insert if missing, move this code to a function
+    else:
+        pass
